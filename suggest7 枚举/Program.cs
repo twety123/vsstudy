1488ec5e8cf699522a3f556394976742531ddc71
twety <twety123@qq.com>
2015-08-27 22:10:10 +0800

    0827

---
+++ suggest7 枚举/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace suggest7_枚举
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Days aa = Days.Saturday;
+            Colors bb = Colors.Black;
+            Console.WriteLine(Days.Saturday.ToString());
+            Console.ReadLine();
+        }
+    }
+
+    enum Days
+    {
+        Saturday = 1, Sunday, Monday, Tuesday
+    }
+
+    enum Colors
+    {
+        Red = 'a', White = 'b', Black = 'c', Yellow = 'd'
+    }
+}
